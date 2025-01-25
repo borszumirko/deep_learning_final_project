51c82efc09693b3d28ffd4451bb6fc05a6ba26de
borszumirko <borszumirko@gmail.com>
2025-01-25 13:11:02 +0100

    add figures and .pth files

--- plot_losses.py
+++ plot_losses.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('celeba_WGAN_GP.csv')
+# Load the data
+data = pd.read_csv('celeba_hq_WGAN_GP.csv')
 
 # Plot the data
 plt.figure(figsize=(10, 6))
-plt.plot(data['discriminator'], label='Discrimintor loss', color='blue')
+plt.plot(data['discriminator'], label='Discriminator loss', color='blue')
 plt.plot(data['generator'], label='Generator loss', color='orange')
 
 # Customize the plot
 plt.title('Discriminator and Generator Losses Over Epochs', fontsize=16)
 plt.xlabel('Epoch', fontsize=14)
-plt.ylabel('BCE Loss', fontsize=14)
+plt.ylabel('Wasserstein loss with gradient penalty', fontsize=14)
 plt.legend(fontsize=12)
 plt.grid(True)
 plt.tight_layout()
+
+# Save the plot as a PDF
+plt.savefig('WGAN_losses_hq.pdf', format='pdf')
 
 # Show the plot
 plt.show()
